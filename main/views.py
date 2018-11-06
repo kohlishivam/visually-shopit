b19f7e81ce3ebaa0074ecf7b47b4699dab315e5a
shivam kohli <kohlishivam5522@gmail.com>
2018-11-06 21:46:54 +0530

    removed path error

--- main/views.py
+++ main/views.py
@@ -60,10 +60,10 @@ def setup_webdriver():
     chrome_exec_shim = "chromedriver"
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = chrome_exec_shim
-    chrome_options.add_argument('--disable-gpu');
-    chrome_options.add_argument('--no-sandbox');
-    chrome_options.add_argument('headless');
-    chrome_options.add_argument('--disable-dev-shm-usage');
+    # chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('headless')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(executable_path='chromedriver', chrome_options=chrome_options)
     return driver
 
