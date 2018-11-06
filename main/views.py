facc8ebfc1bf36c171963cdcde23516882ad23bd
shivam kohli <kohlishivam5522@gmail.com>
2018-11-06 22:13:22 +0530

    removed path error

--- main/views.py
+++ main/views.py
@@ -63,7 +63,7 @@ def setup_webdriver():
     chrome_options.add_argument('--disable-gpu');
     chrome_options.add_argument('--no-sandbox');
     chrome_options.add_argument('headless');
-    chrome_options.add_argument('--disable-dev-shm-usage');
+    # chrome_options.add_argument('--disable-dev-shm-usage');
     driver = webdriver.Chrome(executable_path=chrome_exec_shim, chrome_options=chrome_options)
     return driver
 
