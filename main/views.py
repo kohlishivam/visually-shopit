44701328569dbb88308a4b949b5ba28f38e080e4
shivam kohli <kohlishivam5522@gmail.com>
2018-11-07 00:17:05 +0530

    removed errors

--- main/views.py
+++ main/views.py
@@ -91,7 +91,7 @@ def scrapper(keywords):
     print(url)
     driver.get(url)
     data = driver.page_source
-    # print(data)
+    print(data)
     soup = BS(data,"html.parser")
     divdata = soup.find_all('ul', {"class": "results-base"})
     # print(divdata)
