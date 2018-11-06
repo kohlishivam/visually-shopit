175a271c07056219495c238ec4a5b17d85ede3dc
shivam kohli <kohlishivam5522@gmail.com>
2018-11-06 22:23:54 +0530

    removed path error

--- main/views.py
+++ main/views.py
@@ -64,6 +64,9 @@ def setup_webdriver():
     chrome_options.add_argument('--no-sandbox');
     chrome_options.add_argument('headless');
     chrome_options.add_argument('--disable-dev-shm-usage');
+    chrome_options.add_argument("start-maximized"); 
+    chrome_options.add_argument("disable-infobars");
+    chrome_options.add_argument("--disable-extensions"); 
     driver = webdriver.Chrome(executable_path='/app/.chromedriver/bin/chromedriver', chrome_options=chrome_options)
     return driver
 
